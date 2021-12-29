56e9ac29ce993971ef3b845759a9d74e27574273
Maho Yamada <ymd99mh@gmail.com>
2021-12-28 22:33:42 -0500

    added Skill section in About

--- public/portfolio/script.js
+++
@@ -1,9 +0,0 @@
-// var images = document.getElementsByTagName("img");
-// for(var i = 0; i < images.length; i++) {
-//     var image = images[i];
-//     image.onclick = function(event) {
-//          window.location.href = this.id + '.html';
-//     };
-// }
-
-
